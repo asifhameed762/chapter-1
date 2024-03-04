a5bf289e4353656b9c49ae6d0980dfd8e99a6407
Asif Asif <130058986+asifhameed762@users.noreply.github.com>
2024-03-04 20:06:21 +0500

    chapter 1

---
+++ app.js
@@ -0,0 +1,20 @@
+// alert("Hello World");
+// console.log("hi i am asif");
+// document.write("hi everyone");
+
+// Chapter 1 
+// Q1.
+alert("Welcome To Javascript");
+
+// Q2.
+alert("Error! PLease enter a valid password.");
+
+// Q3.
+alert("Welcome To Javascrript Land...\nHappy Coding!");
+
+// Q4.
+alert("Welcome To Javascript Land...");
+alert("Happy Coding! \nPrevent this page from creating additional dialogs.");
+
+//Q5.
+console.log("Hello... I can run JS through my web browser's console");
